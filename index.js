d904a96f64342f5508e0919eef51b38e7ab6859d
divine dela <divinedela.dd@gmail.com>
2020-03-08 18:37:31 +0000

    add endpoint to receive messages from fb messenger

--- index.js
+++ index.js
@@ -40,4 +40,34 @@ app.get('/webhook', (req, res) => {
         res.sendStatus(403);      
       }
     }
-  });   
+  }); 
+  
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+    let body = req.body;
+  
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+  
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Gets the message. entry.messaging is an array, but 
+        // will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log('webhook event ', webhook_event);
+
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+      });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  
+});
