0fdcd26a51faf76ed846294641fa78a81dc8a297
divine dela <divinedela.dd@gmail.com>
2020-03-08 18:18:33 +0000

    add enpoint for messenger webhook verification

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Get facebook Page access token from environment file
+const { PAGE_ACCESS_TOKEN } = require('./env');
 
 const express = require("express");
 const app = express();
@@ -11,3 +13,31 @@ app.get('/', (request, response) => {
     response.send({message: 'Welcome!'});
 });
 
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN =  '77687768'
+      
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+      
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+  });   
